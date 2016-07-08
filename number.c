f9f20e7f4815829f923405a27c48afd17a183dbd
amutha960097 <amutha960097@gmail.com>
2016-07-08 10:27:18 +0530

    number.c

---
+++ number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter a number:");
+scanf(%D,&num);
+if(num>0)
+{
+printf("given number is positive");
+}
+else if(num=0)
+{
+printf("given number is zero");
+}
+else
+{
+printf("given no is negative");
+}
+}
